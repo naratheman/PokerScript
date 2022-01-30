892f0422b748bb224a339b80e8052f6c30ad2807
naratheman <narada.utorodewo@gmail.com>
2022-01-30 03:09:01 -0800

    Added test files, node dependecies and prettier

---
+++ src/PokerScript.js
@@ -0,0 +1,9 @@
+console.log("Hello World")
+
+export function add(x, y) {
+  return x + y
+}
+
+export function times(x, y) {
+  return x * y
+}
---
+++ test/compiler.test.js
@@ -0,0 +1,22 @@
+import assert from "assert/strict"
+import { add, times } from "../src/PokerScript.js"
+
+describe("The compiler ", () => {
+  describe("has an add function", () => {
+    it("and it should return 4 when adding 2 and 2", () => {
+      assert.deepEqual(add(2, 2), 4)
+    })
+  })
+})
+
+describe("The compiler ", () => {
+  describe("has a times function", () => {
+    it("and it should return 4 when miltiplying 2 and 2", () => {
+      assert.deepEqual(times(2, 2), 4)
+    })
+    it("returns 0 when multiplying by 0", () => {
+      assert.deepEqual(times(2, 0), 0)
+      assert.deepEqual(times(23552, 0), 0)
+    })
+  })
+})
