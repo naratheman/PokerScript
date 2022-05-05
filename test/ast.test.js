41c531ca119c50b8ef7bf9f885bbcb23a75bbc00
evanscianc <34558352+evanscianc@users.noreply.github.com>
2022-05-04 21:59:26 -0700

    91.47 percent coverage for ast.test.js

--- test/ast.test.js
+++ test/ast.test.js
@@ -2,7 +2,6 @@ import assert from "assert"
 import util from "util"
 import ast from "../src/ast.js"
 
-//
 const source = `
 chip x: 10
 constantPressure stringBet y: "hello"
@@ -26,9 +25,34 @@ excuses total <= 15 $.
 hit || miss
 
 flop stringBet names: ["Marvin", "Garrett", "Dr.Toal"]
+
+rude Optional type
+chip? my_optional_int: 4
+
+rude Ternary operator
+hit ? 420 : 64
+
+3 +$
+4 -$
+
+myfunc(4)
+
+hit && miss
+
+2 + 4
+4 - 2
+5 * 6
+60 / 5
+61 % 5
+2 ** 4
+4 + (2*4)
+
+reveal names[1]
+
+4.20
 `
 
-const expected = `   1 | Program statements=[#2,#3,#4,#6,#10,#24,#25,#26,#27]
+const expected = `   1 | Program statements=[#2,#3,#4,#6,#10,#24,#25,#26,#27,#30,#32,#33,#34,#35,#36,#37,#38,#39,#40,#41,#42,#43,#45,(Float,"4.20")]
    2 | VariableDeclaration modifier=undefined type=(Sym,"chip") variable=(Id,"x") initializer=(Int,"10")
    3 | VariableDeclaration modifier=(Sym,"constantPressure") type=(Sym,"stringBet") variable=(Id,"y") initializer=(Str,""hello"")
    4 | ReturnStatement expression=#5
@@ -56,7 +80,24 @@ const expected = `   1 | Program statements=[#2,#3,#4,#6,#10,#24,#25,#26,#27]
   26 | BinaryExpression op='||' left=(Bool,"hit") right=(Bool,"miss")
   27 | VariableDeclaration modifier=undefined type=#28 variable=(Id,"names") initializer=#29
   28 | ArrayType description='[stringBet]' baseType=(Sym,"stringBet")
-  29 | ArrayExpression elements=[(Str,""Marvin""),(Str,""Garrett""),(Str,""Dr.Toal"")]`
+  29 | ArrayExpression elements=[(Str,""Marvin""),(Str,""Garrett""),(Str,""Dr.Toal"")]
+  30 | VariableDeclaration modifier=undefined type=#31 variable=(Id,"my_optional_int") initializer=(Int,"4")
+  31 | OptionalType description='chip?' baseType=(Sym,"chip")
+  32 | Conditional test=(Bool,"hit") consequent=(Int,"420") alternate=(Int,"64")
+  33 | Increment operand=(Int,"3")
+  34 | Decrement operand=(Int,"4")
+  35 | Call callee=(Id,"myfunc") args=[(Int,"4")]
+  36 | BinaryExpression op='&&' left=(Bool,"hit") right=(Bool,"miss")
+  37 | BinaryExpression op='+' left=(Int,"2") right=(Int,"4")
+  38 | BinaryExpression op='-' left=(Int,"4") right=(Int,"2")
+  39 | BinaryExpression op='*' left=(Int,"5") right=(Int,"6")
+  40 | BinaryExpression op='/' left=(Int,"60") right=(Int,"5")
+  41 | BinaryExpression op='%' left=(Int,"61") right=(Int,"5")
+  42 | BinaryExpression op='**' left=(Int,"2") right=(Int,"4")
+  43 | BinaryExpression op='+' left=(Int,"4") right=#44
+  44 | BinaryExpression op='*' left=(Int,"2") right=(Int,"4")
+  45 | PrintStatement argument=#46
+  46 | Subscript array=(Id,"names") index=(Int,"1")`
 
 describe("The AST generator", () => {
   it("produces a correct AST", () => {
